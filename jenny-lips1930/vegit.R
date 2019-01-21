3482ce7dab33e1bbda237342c629948ef934bd8c
Roland Kaiser <kardinal.eros@gmail.com>
2019-01-21 11:07:51 +0100

    added project with 12 plots

---
+++ jenny-lips1930/vegit.R
@@ -0,0 +1,57 @@
+library(vegit)
+library(linktaxa)
+library(vegsoup)
+
+setwd("~/Documents/vegsoup-data/jenny-lips1930")
+
+#	match taxa
+file <- "Jenny-Lips1930Tab1"
+x <- paste0(file, ".csv")
+csv2txt(x, header.rows = 0, vertical = FALSE, width = 13, overwrite = FALSE)
+
+x <- readLines(paste0(file, ".txt"))
+
+z <- read.csv2("~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv",
+	colClasses = "character")
+
+#	NOT RUN:
+if (FALSE) {
+	xx <- extractTaxon(x, row = 3, col = 30)
+	
+	#	seek matches ...
+	yy <- linktaxa(xx, z$taxon, order = FALSE,
+		file = paste0(file, "taxon2standard.csv"),
+		overwrite = FALSE)
+		
+	#	 ... and read edits
+	y <- read.csv(paste0(file, "taxon2standard.csv"),
+		colClasses = "character")
+	
+	#	replace and save to file with suffix
+	r <- replaceTaxon(x, y, z, row = 3, col = 30,
+		overwrite = FALSE, keywords = TRUE,
+		file = paste0(file, "taxon2standard.txt"))
+		
+	###	footer species
+	#	transform to matrix and ...
+	xx <- castFooter(paste0(file, "FooterSpecies.txt"), abundance.first = FALSE)
+	#	 ... save for later use
+	#	write.csv(species(xx), paste0(file, "Footer Species.csv"))
+	
+	#	seek matches ...
+	xy <- linktaxa(xx$abbr, z$taxon, order = FALSE,
+		file = paste0(file, "taxon2standardFooterSpecies.csv"), overwrite = TRUE)	
+	
+	#	... and read edits
+	x <- read.csv(paste0(file, "taxon2standardFooterSpecies.csv"), colClasses = "character")
+			
+	#	... replace
+	a <- z$abbr[match(x$matched.taxon, z$taxon)]
+	
+	#	... and save temporary file
+	#	conetents to be copied to write.csv(species(xx), paste0(file,"FooterSpecies.csv"))
+	write.csv2(a, "tmp.csv")		
+}
+
+#	defaults write com.macromates.TextMate fontLeadingDelta -float -4
+#	zoom 135
