c9bcb6c98d522b25ee453ca93cda274bfbb31945
Roland Kaiser <kardinal.eros@gmail.com>
2019-02-26 14:56:58 +0100

    added project with 9 plots

---
+++ aichinger1933/MakeVegsoup.R
@@ -0,0 +1,50 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/aichinger1933"
+key <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8")$key
+
+#	read prepared digitized table
+file <- file.path(path, "Aichinger1933Tab4taxon2standard.txt")
+x <- xx <- read.verbatim(file, "Nr. der Aufnahme", layers = "@", vertical = FALSE)
+
+X0 <- species(x)[, 1:4]
+
+#	and footer taxa
+file <- file.path(path, "Aichinger1933Tab4Footer species.csv")
+X1 <- species(file, sep = ",")[, 1:4]
+
+X <- bind(X0, X1)
+
+#   sites data including coordinates
+file <- "~/Documents/vegsoup-data/aichinger1933/Aichinger1933Tab4Locations.csv"
+Y <- stackSites(file = file, sep = ",")
+
+# taxonomy reference list
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+# promote to class "Vegsoup"
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet2")
+
+#	unique rownames
+rownames(obj) <- paste(key, "Tab4", sprintf("%01d", as.numeric(rownames(obj))), sep = ":")
+
+#	assign allianceTHL-04A
+
+obj$alliance.code <- "THL-04A"
+obj$alliance <- "Petasition paradoxi Zollitsch ex Lippert 1966"
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "",
+	add.lines = TRUE, table.nr = TRUE)
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
