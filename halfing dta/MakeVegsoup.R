72c3674e93c89c800a1faabac08993387cc9f24a
Roland Kaiser <kardinal.eros@gmail.com>
2020-12-12 11:55:18 +0100

    added project with 7 plots

---
+++ halfing dta/MakeVegsoup.R
@@ -0,0 +1,44 @@
+library(vegsoup)
+require(bibtex)
+
+
+path <- "/Users/roli/Documents/vegsoup-data/halfing dta"
+key <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8")$key
+
+#	cd '/Users/roli/Dropbox/ENNACON/projekte/halfing/dta/relevees'
+#	sudo /Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 species.ods
+#	token 76 is the number of the UTF-8 encoding, 44 the comma, and 34 the double quote character ASCII
+
+file <- file.path(path, "species.csv")
+#	promote to class "Species"
+X <- species(file, sep = ",")#[, 1:4]
+
+table(unique(X$cov))
+
+file <- file.path(path, "sites wide.csv")
+
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ";")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	assign result object
+assign(key, obj)
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE)
+
+write.csv2(taxon(obj), file.path(path, "taxa.csv"))
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
+
