ee7190f8d511243e794b609fbb8d69c13559729e
Roland Kaiser <kardinal.eros@gmail.com>
2021-01-06 12:01:38 +0100

    added project with 192 plots

---
+++ kaiser2005/MakeVegsoup.R
@@ -0,0 +1,43 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/kaiser2005"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
+
+file <- file.path(path, "species.csv")
+#	promote to class "Species"
+X <- species(file, sep = ",")[, 1:4]
+
+file <- file.path(path, "sites wide.csv")
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ",", dec = ".")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "pfadenhauer")
+coordinates(obj) <- ~x+y
+proj4string(obj) <- CRS("+init=epsg:31258")
+obj <- spTransform(obj, CRS("+init=epsg:4326"))
+
+#	order layer
+layers(obj) <- c("sl", "hl", "ml")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	add citation
+obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE, select = "richness")
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
