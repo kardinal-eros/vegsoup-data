f39465e13960f84b792763aefcf713a414bf8c0e
Roland Kaiser <kardinal.eros@gmail.com>
2021-01-06 14:27:08 +0100

    added project with 30 plots

---
+++ etrachsee-prebersee dta/MakeVegsoup.R
@@ -0,0 +1,40 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/etrachsee-prebersee dta"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
+
+file <- file.path(path, "species wide.csv")
+#	promote to class "Species"
+X <- stackSpecies(file = file)[, c(1:4)]
+
+file <- file.path(path, "sites wide.csv")
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ";")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+#	order layer
+layers(obj)	 <- c("tl1", "tl2", "sl", "hl", "ml")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	add citation
+obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE, select = "richness")
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
---
+++ etrachsee-prebersee dta/README.md
@@ -0,0 +1,17 @@
+About this data set
+==================
+
+This is a data set from an undergraduate field course of the University of Salzburg held at Lungau (Salzburg, Austria) supervised by Paul Heislelmayer and Verena Meroth.
+
+Topics
+=======
+
+* Methods in phytosociology
+
+* springs fens and confiverous forests
+
+
+Authors
+=======
+
+Information about participating students is missing.
