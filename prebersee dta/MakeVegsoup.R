ec691834049dd904e1630038d543ba931c4f751d
Roland Kaiser <kardinal.eros@gmail.com>
2021-01-06 14:51:22 +0100

    added project with 26 from previous subfolder

--- prebersee dta/MakeVegsoup.R
+++ prebersee dta/MakeVegsoup.R
@@ -1,65 +1,64 @@
 require(vegsoup)
 require(raster)
 
-#	data from course in year 2012 (Paul Heiselmayer and Verena Meroth)
+path <- "~/Documents/vegsoup-data/prebersee dta"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
 
-file <- "~/Documents/vegsoup-data/prebersee dta/2012/species wide.csv"
-X <- stackSpecies(file = file)[, c(1:4)]
+file <- file.path(path, "species.csv")
+#	promote to class "Species"
+X <- species(file, sep = ";")[, c(1:4)]
 
-file <- "~/Documents/vegsoup-data/prebersee dta/2012/sites wide.csv"
-Y <- stackSites(file = file)
-
-file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
-XZ <- SpeciesTaxonomy(x = X, file.y = file)
-
-us2012 <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
-
-save(us2012, file = "~/Documents/vegsoup-data/prebersee dta/ps2012.rda")
-rm(list = ls()[-grep("us2012", ls(), fixed = TRUE)])
-
-
-#	data from course in year 2013 (Roland Kaiser and Verena Meroth)
-
-file <- "~/Documents/vegsoup-data/prebersee dta/2013/species.csv"
-X <- species(file, sep = ";")#[, c(1:4)]
-
-file <- "~/Documents/vegsoup-data/prebersee dta/2013/sites wide.csv"
-Y <- stackSites(file = file)
+file <- file.path(path, "sites wide.csv")
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ";")
 
 file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
-XZ <- SpeciesTaxonomy(x = X, file.y = file)
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
 
-us2013 <- Vegsoup(XZ, Y, coverscale = "as.is")
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "pa")
 
 #	complement missing values from interpolated data
-i <- is.na(us2013$ph)
-
-#	prediction grid have Global Mercator projection
-#us2013 <- spTransform(us2013, CRS("+init=epsg:3857"))
+i <- is.na(obj$ph)
 
 #	measurement taken from wells
-r <- brick("~/Documents/vegsoup-data/prebersee dta/2013/variables.tif")
+file <- file.path(path, "variables.tif")
+r <- brick(file)
 names(r) <- c("ph", "conductivity", "watertable")
-r <- as.data.frame(extract(r, us2013))
-us2013$ph[i] <- round(r$ph[i], 1)
-us2013$conductivity[i] <- round(r$conductivity[i], 0)
-us2013$watertable[i] <- round(r$watertable[i], 0) * -1
+r <- as.data.frame(extract(r, obj))
+obj$ph[i] <- round(r$ph[i], 1)
+obj$conductivity[i] <- round(r$conductivity[i], 0)
+obj$watertable[i] <- round(r$watertable[i], 0) * -1
 #	we need to subtract the well's height for measurements
-us2013$watertable[!i] <- us2013$watertable[!i] - us2013$wellheight[!i]
-
+obj$watertable[!i] <- obj$watertable[!i] - obj$wellheight[!i]
 
 #	interpolated slope and aspect map from high precision GNSS measurements
 #	taken at each plot
-r <- raster("~/Documents/vegsoup-data/prebersee dta/2013/slope.tif")
-us2013$slope <- extract(r, us2013)
+file <- file.path(path, "slope.tif")
+r <- raster(file)
+obj$slope <- extract(r, obj)
 
-r <- raster("~/Documents/vegsoup-data/prebersee dta/2013/aspect.tif")
-us2013$aspect <- compass(extract(r, us2013))
+file <- file.path(path, "aspect.tif")
+r <- raster(file)
+obj$aspect <- compass(extract(r, obj))
 
-#us2013 <- spTransform(us2013, CRS("+init=epsg:4326"))
+#	order layer
+layers(obj)	 <- c("sl", "hl", "ml")
 
-save(us2013, file = "~/Documents/vegsoup-data/prebersee dta/ps2013.rda")
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	add citation
+obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE, select = "richness")
 
 #	tidy up
-
-rm(list = ls()[-c(grep("us2012", ls(), fixed = TRUE), grep("us2013", ls(), fixed = TRUE))])
+rm(list = ls()[-grep(key, ls())])
--- prebersee dta/README.md
+++ prebersee dta/README.md
@@ -1,1 +1,26 @@
-See the subfolders for details.
+About this dat set
+==================
+
+This is a data set from an undergraduate field course of the University of Salzburg held at Lungau (Salzburg, Austria) supervised by Roland Kaiser and Franz Stross.
+
+
+Topics
+======
+
+* Vegetation of rich fens
+
+* Mire hydrology
+
+* Testing some indicator species for base-rich mires
+
+
+Authors
+=======
+
+Group 1: Alexander Fraueneder, Franz Aschenberger, Wolfgang Hermann, Liedl Sandra, Schramm David and Zezula Sebastian Fischer
+
+Group 4: Ganser Maximilian, Grasegger Tobias, Radosai Sarah, Sattler Thomas, Theuer Johannes and Schlager Martin
+
+Group 6: Dominik Ankel, René Eggers, Philippe Pech, Michael Kohlberger, Franz Stross, Philipp Wenta
+
+Group 0: additional plots by Roland Kaiser
