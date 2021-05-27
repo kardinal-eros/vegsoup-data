131a61b67c52618444e74b9b2e34b00861821e36
Roland Kaiser <kardinal.eros@gmail.com>
2021-05-27 15:37:58 +0200

    added project with 11 plots

---
+++ reslberg dta/MakeVegsoup.R
@@ -0,0 +1,41 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/reslberg dta"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
+key <- bib$key
+key <- key[[1]]
+
+file <- "~/Documents/vegsoup-data/reslberg dta/species wide.csv"
+X <- stackSpecies(file = file, sep = ",")[, 1:4]
+
+file <- "~/Documents/vegsoup-data/reslberg dta/sites wide.csv"
+Y <- stackSites(file = file, sep = ",", dec = ".")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+XZ <- SpeciesTaxonomy(x = X, file.y = file)
+
+obj <- Vegsoup(XZ, Y, coverscale = "pfadenhauer")
+
+#	order layer
+layers(obj)	 <- c("hl", "ml")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	add citation
+obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "",
+	add.lines = TRUE, table.nr = TRUE)
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
+
+
