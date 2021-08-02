2a49c0d491ba1b17532114d5bedc88bdf42d7882
Roland Kaiser <kardinal.eros@gmail.com>
2021-08-02 18:48:46 +0200

    added new project with 2 plots

---
+++ unterirnprechting dta/MakeVegsoup.R
@@ -0,0 +1,41 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/unterirnprechting dta"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
+
+#	cd '/Users/roli/Documents/vegsoup-data/wagrain dta'
+#	sudo /Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 species.ods
+#	token 76 is the number of the UTF-8 encoding, 44 the comma, and 34 the double quote character ASCII
+
+file <- file.path(path, "species.csv")
+#	promote to class "Species"
+X <- species(file, sep = ",")[, 1:4]
+
+file <- file.path(path, "sites wide.csv")
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ",")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	add citation
+obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE, select = "richness")
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
