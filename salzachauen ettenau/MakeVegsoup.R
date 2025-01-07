6d229c757c02f1a49b342a33a1826aded9bb546a
Roland Kaiser <kardinal.eros@gmail.com>
2025-01-07 11:46:39 +0100

    added project with 16 plots

---
+++ salzachauen ettenau/MakeVegsoup.R
@@ -0,0 +1,48 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/salzachauen ettenau"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
+
+#	cd '/Users/roli/Dropbox/ENNACON/projekte/gebietsbetreuung/dta/relevees'
+#	/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 species.ods
+#	token 76 is the number of the UTF-8 encoding, 44 the comma, and 34 the double quote character ASCII
+
+file <- file.path(path, "species.csv")
+#	promote to class "Species"
+X <- species(file, sep = ",")[, 1:4]
+
+file <- file.path(path, "sites wide.csv")
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ";", dec = ",")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	order based on plot and year
+obj$year <- format(as.Date(obj$date), "%Y")
+obj$plot2 <- sapply(strsplit(rownames(obj), "-"), head, 1)
+
+obj <- obj[ order(obj$plot2, obj$year) ]
+
+#	add citation
+#obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+#obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE, select = "richness")
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
+
