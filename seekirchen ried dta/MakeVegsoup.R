22860213dbc853fd921175538b536dd104f07f3f
Roland Kaiser <kardinal.eros@gmail.com>
2025-12-31 13:44:40 +0100

    added project with 13 plots

---
+++ seekirchen ried dta/MakeVegsoup.R
@@ -0,0 +1,41 @@
+library(vegsoup)
+require(bibtex)
+
+path <- "/Users/roli/Documents/vegsoup-data/seekirchen ried dta"
+bib <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8"); key <- bib$key
+
+file <- file.path(path, "species.csv")
+#	promote to class "Species"
+X <- species(file, sep = ",")[, 1:4]
+
+file <- file.path(path, "sites wide.csv")
+
+#	promote to class "Sites"
+Y <- stackSites(file = file, sep = ",")
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+#	promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+
+#	build "Vegsoup" object
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+#	order layer
+layers(obj)	 <- c("hl", "sl", "tl2", "tl1")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	add citation
+obj$author <- ifelse(length(bib$author) > 1, paste0(as.character(bib$author), collapse = ", "), as.character(bib$author))
+obj$citation <- format(bib, style = "text")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE, select = "richness")
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
